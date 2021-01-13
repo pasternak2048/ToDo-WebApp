92998ff588e9df6a3f6b1cb564be012fcb361825
pasternak2048 <pasternak2048@gmail.com>
2021-01-13 12:52:56 +0200

    Added data context model

---
+++ ToDo-App/ToDo-App/Models/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ToDo> ToDos { get; set; }
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+    }
+}
