2dfc1fac9f4fcd7faafcabb59d61517b30b4d1ac
pasternak2048 <pasternak2048@gmail.com>
2021-01-13 13:27:24 +0200

    Added two connections to appsettings.json, added service AddDbContext to Startup

--- ToDo-App/ToDo-App/Startup.cs
+++ ToDo-App/ToDo-App/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ToDo_App.Models;
 
 namespace ToDo_App
 {
@@ -23,7 +25,11 @@ namespace ToDo_App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ToDoContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
