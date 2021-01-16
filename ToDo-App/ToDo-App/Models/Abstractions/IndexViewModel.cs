145ec4a40c20b1b2d230a72892a8810d8f9f761e
pasternak2048 <pasternak2048@gmail.com>
2021-01-16 17:13:39 +0200

    Created models PageViewModel, IndexViewModel<T>

---
+++ ToDo-App/ToDo-App/Models/Abstractions/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Models.Abstractions
+{
+    public class IndexViewModel<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
---
+++ ToDo-App/ToDo-App/Models/PageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_App.Models
+{
+    public class PageViewModel
+    {
+        public int PageNumber { get; }
+        public int TotalPages { get; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+
+        public PageViewModel(int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+    }
+}
