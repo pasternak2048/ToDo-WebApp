67f300e2a3f7091f63f60c69516a8f7b0c870383
pasternak2048 <pasternak2048@gmail.com>
2021-01-21 14:02:02 +0200

    Localized User model, moved language button to Header

--- ToDo-App/ToDo-App/Models/User.cs
+++ ToDo-App/ToDo-App/Models/User.cs
@@ -10,15 +10,24 @@ namespace ToDo_App.Models
     {
         public int Id { get; set; }
 
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Incorrect Email")]
+        [Required(ErrorMessage = "PropertyEmailRequiredError")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "IncorrectEmailError")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "PropertyPasswordRequiredError")]
+        [StringLength(maximumLength: 16, ErrorMessage = "PropertyPasswordLengthError")]
         public string Password { get; set; }
 
-        [StringLength(maximumLength: 50, ErrorMessage = "The 'Last Name' field must not exceed 50 symbols")]
+        [Required(ErrorMessage = "PropertyLastNameRequiredError")]
+        [StringLength(maximumLength: 50, ErrorMessage = "PropertyLastNameLengthError")]
         public string LastName { get; set; }
 
-        [StringLength(maximumLength: 50, ErrorMessage = "The 'First Name' field must not exceed 50 symbols")]
+        [Required(ErrorMessage = "PropertyFirstNameRequiredError")]
+        [StringLength(maximumLength: 50, ErrorMessage = "PropertyFirstNameLengthError")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "PropertyAddressRequiredError")]
+        [StringLength(maximumLength: 50, ErrorMessage = "PropertyAddressLengthError")]
         public string Address { get; set; }
         public DateTimeOffset DateOfRegistration { get; set; }
         public int? RoleId { get; set; }
